501434f5cba5112975d3618174dfa1f7c9358736
Andrew Nordlund <andrew.nordlund@hrsdc-rhdcc.gc.ca>
2022-05-20 18:05:53 -0400

    WIP:Started the structure of the index.js file, and added lang attributes to the language switcher.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
   console.log("Hello there, world");
 });
+
+// fixAllPages fixes everything that appears across the Virtual Platform
+function fixAllPages () {
+	fixLangSwitcher();
+} // End of fixAllPages
+
+// fixLangSwitcher
+function fixLangSwitcher() {
+	// Andrew Nordlund - Add lang attributes to the language switcher:
+	$('#languages-modal a.list-group-item.list-group-item-action:contains("Français")').html('<span lang="en">French</span> (<span lang="fr">Français</span>)');
+} // End of fixAllPages
