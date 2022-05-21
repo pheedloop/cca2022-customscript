2c03d507e3031f66dc625744a316fb2bd5cf03f0
Andrew Nordlund <andrew.nordlund@hrsdc-rhdcc.gc.ca>
2022-05-20 22:33:46 -0400

    WIP: Made the Request New Password modal much more accessible by making a div an h2, the text a label, setting focus on the email input, and then replacing focus to the RESET PASSWORD link when the email/Yikes/Success modals close.  Thankfully it's cuz they all use swal, which has issues, but made this one fix easier.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	console.log("Hello there, world");
 	fixAllPages ();
+
+	// If on the login page:
+	if (document.location.pathname == "/CCA2022/login/auth/") fixLoginPage();
 });
 
 // fixAllPages fixes everything that appears across the Virtual Platform
@@ -19,3 +22,27 @@ function fixLangModal() {
 	$('#languages-modal>div>div.modal-content>div.modal-header>h5:first-child').replaceWith("<h2 class='h5'>" + $('#languages-modal>div>div.modal-content>div.modal-header>h5:first-child').html() + "</h2>");
 	$('#languages-modal>div>div.modal-content>div.modal-header>button.close').attr("aria-label", "Close");
 } // End of fixLangModal
+
+function fixLoginPage() {
+	fixRequestPasswordModal();	
+} // End of fixLoginPage
+
+function fixRequestPasswordModal () {
+	$('div#login-form-container>div.mt-3>a:first').click(function () {
+		setTimeout(function () {
+			$('div.swal-title').replaceWith("<h2 id='reqNewPasswordH2' class='swal-title'>" + $('div.swal-title').html() + "</h2>");
+			$('div.swal-text').replaceWith("<label class='swal-text' for='reqNewPasswordTxt' id='reqNewPasswordLbl'>" + $('div.swal-text').html() +"</label>");
+			$('input.swal-content__input').attr("id", "reqNewPasswordTxt").attr("autocomplete","email");
+    
+			let observer = new MutationObserver(mutationRecords => {
+				for (let mutation of mutationRecords) {
+					if (mutation.type === 'attributes' && mutation.oldValue.match(/swal-overlay--show-modal/)) {
+						$('div#login-form-container>div.mt-3>a:first').focus();
+					}
+				}
+			});
+			let nd = $('div.swal-overlay--show-modal')[0];
+			observer.observe(nd, {attributes : true, attributeFilter: ['class'], attributeOldValue : true});
+		}, 200);
+	});
+} // End of fixRequestPasswordModal
