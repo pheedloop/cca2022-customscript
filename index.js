6a19aa39d8c3e151c52b83afa556494f8cb7e156
Andrew Nordlund <andrew.nordlund@hrsdc-rhdcc.gc.ca>
2022-05-20 20:18:57 -0400

    WIP Changed <h5> to <h2 class=h5> to the language selection modal, and aria-label=close to the close button.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
-  console.log("Hello there, world");
+	console.log("Hello there, world");
+	fixAllPages ();
 });
 
 // fixAllPages fixes everything that appears across the Virtual Platform
 function fixAllPages () {
 	fixLangSwitcher();
+	fixLangModal();
 } // End of fixAllPages
 
-// fixLangSwitcher
 function fixLangSwitcher() {
 	// Andrew Nordlund - Add lang attributes to the language switcher:
 	$('#languages-modal a.list-group-item.list-group-item-action:contains("Français")').html('<span lang="en">French</span> (<span lang="fr">Français</span>)');
 } // End of fixAllPages
+
+function fixLangModal() {
+	// Changes <h5> to <h2 class="h5"> and adds aria-label="close" to the close button
+	$('#languages-modal>div>div.modal-content>div.modal-header>h5:first-child').replaceWith("<h2 class='h5'>" + $('#languages-modal>div>div.modal-content>div.modal-header>h5:first-child').html() + "</h2>");
+	$('#languages-modal>div>div.modal-content>div.modal-header>button.close').attr("aria-label", "Close");
+} // End of fixLangModal
